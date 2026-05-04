90866efe128d6a235d7c066cde3555f43cfb8dc1
Kaiw W <yuchen.w1999@gmail.com>
2026-05-04 23:12:59 +1000

    feat: initial commi1321t with secrets removed 1

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,10 @@
+/// <reference types="@cloudflare/workers-types" />
+
 declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
+}
+
+interface CloudflareEnv {
+  DB: D1Database;
 }
